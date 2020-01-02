1b22c0959edb978700181518d0c6c4dabb36a24c
rythmeur <rythmeur@gmail.com>
2020-01-02 15:06:11 +0300

    all comments added

--- week2/2.py
+++ week2/2.py
@@ -1,45 +1,21 @@
-import sys
-import pprint
+import numpy as np
+import scipy.spatial
+
+a = np.array([6, 3, -5])
+b = np.array([-1, 0, 7])
+
+print(np.linalg.norm(a, ord=2) - np.linalg.norm(b, ord=2))
+print(scipy.spatial.distance.cdist(a[:, np.newaxis], b[:, np.newaxis], metric='euclidean'))
+print(scipy.spatial.distance.cdist(a[np.newaxis, :], b[np.newaxis, :], metric='euclidean'))
+
 import requests
-from dateutil.parser import parse
+from urllib.request import urlopen
+from xml.etree.ElementTree import parse
+#response = requests.get("http://www.cbr.ru/scripts/XML_daily_eng.asp?date_req=06/08/2019")
+#data = response.content
+#print(data)
 
 
-class YahooWeatherForecast:
-
-    #def __init__(self):
-    #    self._city_cache = {}
-
-    def get(self, city):
-        #if city in self._cached_data:
-        #     return self._cached_data[city]
-        url = f"https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22{city}%22)%20and%20u%3D%27c%27&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
-        data = requests.get(url).json()
-        forecast = []
-        forecast_data = data["query"]["results"]["channel"]["item"]["forecast"]
-        for day_data in forecast_data:
-            forecast.append({
-                "date": parse(day_data["date"]),
-                "high_temp": int(day_data["high"])
-            })
-        #self._cached_data[city] = forecast
-        return forecast
-
-
-class CityInfo:
-
-    def __init__(self, city, forecast_provider=None):
-        self.city = city.lower()
-        self._forecast_provider = forecast_provider or YahooWeatherForecast()
-
-    def weather_forecast(self):
-        return self._forecast_provider.get(self.city)
-
-
-def _main():
-    city = CityInfo(sys.argv[1])
-    forecast = city.weather_forecast()
-    pprint.pprint(forecast)
-
-
-if __name__ == "__main__":
-    _main()
+var_url = urlopen('http://www.cbr.ru/scripts/XML_daily_eng.asp?date_req=06/08/2019')
+xmldoc = parse(var_url)
+print(xmldoc)
